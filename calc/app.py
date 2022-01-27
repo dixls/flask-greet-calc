31972c8f5fe3f74e0bb9fcfcec952573db5dff51
pixls <tha.pixls@gmail.com>
2022-01-27 11:54:44 -0500

    finished exercises

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,47 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_page():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    result = add(a,b)
+    return f"{result}"
+
+@app.route('/sub')
+def sub_page():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    result = sub(a,b)
+    return f"{result}"
+
+@app.route('/mult')
+def mult_page():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    result = mult(a,b)
+    return f"{result}"
+
+@app.route('/div')
+def div_page():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    result = div(a,b)
+    return f"{result}"
+
+operations = {
+    'add': add,
+    'sub': sub,
+    'mult': mult,
+    'div': div
+}
+
+@app.route('/math/<opp>')
+def math_page(opp):
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    result = operations[opp](a,b)
+    return f"{result}"
